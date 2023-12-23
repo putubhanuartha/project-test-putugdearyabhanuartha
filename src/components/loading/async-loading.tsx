4dd64adb435aa1cec08b4fd64b42f21ff0b40a37
putubhanuartha <putuaryabhanu03@gmail.com>
2023-12-23 15:48:01 +0700

    feat/ bouncing animation on loading

--- src/components/loading/async-loading.tsx
+++ src/components/loading/async-loading.tsx
@@ -1,7 +1,7 @@
 const AsyncLoading = () => {
 	return (
 		<div className="min-h-[50vh] flex pt-20">
-			<p className="m-auto text-xl font-semibold w-fit">
+			<p className="m-auto animate-bounce text-2xl font-semibold w-fit">
 				Loading Resources ...
 			</p>
 		</div>
